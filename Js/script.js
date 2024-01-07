ac093dcea57ceb13a2241138269030c3ab23205a
Builder <148814734+Waffel0@users.noreply.github.com>
2024-01-07 10:41:31 -0300

    Versao 1.0

---
+++ Js/script.js
@@ -0,0 +1,40 @@
+const pipe = document.querySelector('.pipe');
+const mario = document.querySelector('.mario');
+
+
+function jump() {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+
+        mario.classList.remove('jump');
+
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+
+    if (pipePosition <= 80 && pipePosition > 0 && marioPosition < 105) {
+       
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = 'imagens/game-over.png'
+        mario.style.width = '60px'
+        mario.style.marginLeft = '25px'
+
+        clearInterval(loop)
+
+    };
+
+}, 10);
+
+document.addEventListener('keydown', jump);
+document.addEventListener('touchstart', jump);
